0e265669cb41801784187e245c93cc3e58827e8f
Ying-Pan9 <yingnianp@nycstudents.net>
2026-04-15 15:52:13 +0000

    java 48 cw

---
+++ Java-48-CW/script.js
@@ -0,0 +1,31 @@
+let data, info; // global variables
+
+async function init(){  
+  let link = "https://bookish-pancake-g4rq9pwvvr7wcv6rj.github.dev/"; // Insert your Dev URL from Activity 47. The activity (DB server) must be running in a separate tab.
+  let route= "/employees";
+
+  /*
+  For the problems below, use the DB server you created for Activity 47 and start it. If you did not complete the first 2 problems of activity 47, then use the database server link given in Lesson 48-Discussion solution (today's lesson).
+  Make sure the link (forwarded Address from the "Port" tab in Codespaces) has been made PUBLIC.
+  Install the "Live Server" extension. Right click your index.html file and choose 'Open with Live Server' to view the web page.
+  */
+  
+  // Problem 1: Add the link to your database server and the route above.
+
+  
+  // Problem 2: Write the JS code in the function init() and get the employee's table from your server as an array of JSON.
+  info = await fetch (link+route);
+  data = await info.json();
+  
+  // Problem 3: For each employee, display their first name, last name and birthdate to the console.
+  for(let i = 0; i<employees.length; i++){
+    let employee = employees[i];
+    console.log("First name: "+employee.FirstName);
+    console.log("Last name: " +employee.LastName);
+    console.log("Birth date: " + employee.BirthDate);
+    console.log("***************"); 
+
+  }
+  
+
+}
